3722dbd2e7b41c3de4e0c33e2ca8466cfbcb8bc9
Dennis <dennismarumahoko@gmail.com>
2024-01-23 15:56:54 +0200

    chore: initial commit

--- bin/aws-rest-api.ts
+++ bin/aws-rest-api.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { AwsRestApiStack } from '../lib/aws-rest-api-stack';
+import * as cdk from "aws-cdk-lib";
+import { AwsRestApiStack } from "../lib/aws-rest-api-stack";
 
 const app = new cdk.App();
-new AwsRestApiStack(app, 'AwsRestApiStack');
+const region = "us-east-1";
+const account = "313002366985";
+new AwsRestApiStack(app, "AwsRestApiStack", { env: { region, account } });
